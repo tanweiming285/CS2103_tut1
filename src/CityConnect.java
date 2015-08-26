07ceaf717f1bf2f8e370dbc406057ec10e5c629b
tanweiming285 <tanweiming285@gmail.com>
2015-08-26 21:08:06 +0800

    Added spaces to make an unnecessary commit

--- src/CityConnect.java
+++ src/CityConnect.java
@@ -100,6 +100,8 @@ public class CityConnect {
 	public static void main(String[] args) {
 		showToUser(WELCOME_MESSAGE);
 		while (true) {
+			
+			
 			System.out.print("Enter command:");
 			String userCommand = scanner.nextLine();
 			String feedback = executeCommand(userCommand);
