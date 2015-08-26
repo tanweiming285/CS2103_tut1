3310a653d2a3f424881a92df278611a973ccec4f
tanweiming285 <tanweiming285@gmail.com>
2015-08-26 22:33:10 +0800

    Added code in CityConnectTest to check for null strings.

--- src/CityConnectTest.java
+++ src/CityConnectTest.java
@@ -1,18 +1,31 @@
 import static org.junit.Assert.*;
 
+import java.util.Scanner;
+
 import org.junit.Test;
+
+import CityConnect.COMMAND_TYPE;
 
 
 public class CityConnectTest {
 
 	@Test
 	public void testMain() {
-		fail("Not yet implemented");
+		CityConnect test = new CityConnect();
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter command:");
+		String userCommand = scanner.nextLine();
+		String feedback = test.executeCommand(userCommand);
+		//Testing to make sure executecommand does not return null
+		assertNotNull(feedback);
 	}
 
 	@Test
 	public void testExecuteCommand() {
-		fail("Not yet implemented");
+		CityConnect test = new CityConnect();
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter command:");
+		String userCommand = scanner.nextLine();	
 	}
 
 }
